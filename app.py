762554851bfc9fc45e8976bcb397d19ce3b47423
dotzero <mail@dotzero.ru>
2016-03-02 22:06:12 +0300

    Fix redis calling

--- app.py
+++ app.py
@@ -10,9 +10,7 @@ from werkzeug.routing import BaseConverter
 app = Flask(__name__)
 app.config['APP_PATH'] = os.path.dirname(os.path.realpath(__file__))
 app.config['WORDS_PATH'] = os.path.join(app.config['APP_PATH'], 'words.txt')
-app.config['REDIS_URL'] = "redis://glocalhost:6379/1"  # redis://:password@localhost:6379/0
-
-redis_store = FlaskRedis(app)
+app.config['REDIS_URL'] = "redis://:password@localhost:6379/0"
 
 
 class RegexConverter(BaseConverter):
@@ -24,17 +22,21 @@ class RegexConverter(BaseConverter):
 app.url_map.converters['regex'] = RegexConverter
 
 
+def get_redis():
+    return FlaskRedis(app)
+
+
 def padkey(padname, prefix='pad'):
     return '%s:%s' % (prefix, padname)
 
 
 @app.route('/<regex("\w+"):padname>', methods=['GET'])
 def get(padname):
-    content = redis_store.get(padkey(padname))
+    content = get_redis().get(padkey(padname))
     if not content:
         content = ''
 
-    return render_template('main.html', padname=padname, content=content)
+    return render_template('main.html', padname=padname, content=content.decode('utf-8'))
 
 
 @app.route('/<regex("\w+"):padname>', methods=['POST'])
@@ -43,7 +45,7 @@ def set(padname):
     if not content:
         abort(401)
 
-    redis_store.set(padkey(padname), content)
+    get_redis().set(padkey(padname), content)
     return jsonify(message='ok', padname=padname)
 
 
@@ -52,7 +54,7 @@ def main():
     words = open(app.config['WORDS_PATH'], 'r').read().splitlines()
     word = words.pop(random.randrange(len(words)))
 
-    while redis_store.exists(padkey(word)):
+    while get_redis().exists(padkey(word)):
         word += str(random.randrange(10))
 
     return redirect('/%s' % word)
